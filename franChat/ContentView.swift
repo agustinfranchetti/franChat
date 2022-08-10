4f416d05f90ee3eee23a4f84a5d0503ebec91480
agustinfranchetti <agustinfranchetti@gmail.com>
2022-08-10 16:31:51 -0300

    Initial Commit

---
+++ franChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  franChat
+//
+//  Created by Agustin Franchetti on 10/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ franChat/franChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  franChatApp.swift
+//  franChat
+//
+//  Created by Agustin Franchetti on 10/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct franChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
